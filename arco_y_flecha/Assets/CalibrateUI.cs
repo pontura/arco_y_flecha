ad5791da6b093b14b68b4518fad9855f8458a0c1
Agustin Pontura <pontura@gmail.com>
2025-04-24 12:10:15 -0300

    save calibration + filtro por tiempo

--- arco_y_flecha/Assets/CalibrateUI.cs
+++ arco_y_flecha/Assets/CalibrateUI.cs
@@ -7,6 +7,32 @@ public class CalibrateUI : MonoBehaviour
 
     public void Init()
     {
+        CheckCalibrationSaved();
+    }
+    void CheckCalibrationSaved()
+    {
+        print("Checking Calibrations...!");
+        if (PlayerPrefs.HasKey("calibrate_1_x"))
+        {
+            print("CheckCalibrationSaved!");
+            int x_1 = PlayerPrefs.GetInt("calibrate_1_x");
+            int y_1 = PlayerPrefs.GetInt("calibrate_1_y");
+            int x_2 = PlayerPrefs.GetInt("calibrate_2_x");
+            int y_2 = PlayerPrefs.GetInt("calibrate_2_y");
+            int x_3 = PlayerPrefs.GetInt("calibrate_3_x");
+            int y_3 = PlayerPrefs.GetInt("calibrate_3_y");
+            int x_4 = PlayerPrefs.GetInt("calibrate_4_x");
+            int y_4 = PlayerPrefs.GetInt("calibrate_4_y");
+
+            GameManager.Instance.quadUtils.Set(
+                new Vector2(x_1, y_1),
+                new Vector2(x_2, y_2),
+                new Vector2(x_3, y_3),
+                new Vector2(x_4, y_4)
+                );
+
+            Events.CalibrationDone();
+        }
     }
     public void InitCalibrate()
     {
@@ -20,6 +46,18 @@ public class CalibrateUI : MonoBehaviour
 
         if (id >= points.Length)
         {
+            PlayerPrefs.SetInt("calibrate_1_x", (int)points[0].value.x);
+            PlayerPrefs.SetInt("calibrate_1_y", (int)points[0].value.y);
+
+            PlayerPrefs.SetInt("calibrate_2_x", (int)points[1].value.x);
+            PlayerPrefs.SetInt("calibrate_2_y", (int)points[1].value.y);
+
+            PlayerPrefs.SetInt("calibrate_3_x", (int)points[2].value.x);
+            PlayerPrefs.SetInt("calibrate_3_y", (int)points[2].value.y);
+
+            PlayerPrefs.SetInt("calibrate_4_x", (int)points[3].value.x);
+            PlayerPrefs.SetInt("calibrate_4_y", (int)points[3].value.y);
+
             GameManager.Instance.quadUtils.Set(
                 points[0].value,
                 points[1].value,
--- arco_y_flecha/Assets/OSCExampleReceiver.cs
+++ arco_y_flecha/Assets/OSCExampleReceiver.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using extOSC;
-using static UnityEngine.Rendering.DebugUI;
+using System.Collections.Generic;
+using System;
 
 public class OSCExampleReceiver : MonoBehaviour
 {
@@ -8,61 +9,110 @@ public class OSCExampleReceiver : MonoBehaviour
     [SerializeField] TMPro.TMP_Text field;
     public OSCReceiver receiver;
 
-    float delayTimer = 1;
-
+    float filterDuration = 0.25f;
     int offset = 10;
+    string key = "objeto";   
 
-    string key = "objeto1";
+    public List<ObjectData> data;
 
-    public Vector2 pos_1;
-    public Vector2 pos_2;
-    public Vector2 pos_3;
-
-    float last_pos_1_timer;
-    float last_pos_2_timer;
-    float last_pos_3_timer;
-
-    void Start()
+    [Serializable]
+    public class ObjectData
     {
-        inputManager = GetComponent<InputManager>();
-        print("OSCExampleReceiver 1");
-        
-        //receiver.Bind("/pos_x", OnReceiveMessageX);
-        receiver.Bind("/" + key, OnPos1);
+        public Vector2 pos;
+        public Vector2 last_pos;
+        public float last_pos_timer;
     }
-    void OnPos1(OSCMessage message)
+
+    private void Update()
     {
-        if (last_pos_1_timer != 0 && last_pos_1_timer + delayTimer > Time.time) return;
-        last_pos_1_timer = Time.time;
-        if (message.Address == "/" + key)
+        if(Input.GetMouseButton(0))
         {
-            Vector2 newValue = Vector2.zero;
-            for (int i = 0; i < message.Values.Count; i++)
-            {
-                float value = (float)message.Values[i].FloatValue;
-                Debug.Log("Recibido float: " + value);
-                //field.text += value.ToString();
-                newValue[i] = value;
-            }
-            if(pos_1 == Vector2.zero || Vector2.Distance(newValue, pos_1)>offset)
-            {
-                pos_1 = newValue;
-                last_pos_1_timer = Time.time;
-                inputManager.OnHit(newValue);
-                field.text = newValue.ToString();
-            }
+            data[0].pos.x = Input.mousePosition.x;
+            data[0].pos.y = Input.mousePosition.y;
+            CheckPos(data[0]);
         }
     }
-    //void OnReceiveMessageX(OSCMessage message)
+
+    public void Start()
+    {
+        print("Start");
+
+        for (int i = 0; i < 3; i++)
+            data.Add (new ObjectData ());
+
+        inputManager = GetComponent<InputManager>();
+        print("OSCExampleReceiver");
+
+        receiver.Bind("/" + key + 1 + "x", OnPos1X);
+        receiver.Bind("/" + key + 1 + "y", OnPos1Y);
+
+        receiver.Bind("/" + key + 2 + "x", OnPos2X);
+        receiver.Bind("/" + key + 2 + "y", OnPos2Y);
+
+        receiver.Bind("/" + key + 3 + "x", OnPos3X);
+        receiver.Bind("/" + key + 3 + "y", OnPos3Y);
+    }
+
+    void OnPos1X(OSCMessage message) { OnPosX(data[0], message); }
+    void OnPos1Y(OSCMessage message) { OnPosY(data[0], message); }
+
+    void OnPos2X(OSCMessage message) { OnPosX(data[1], message); }
+    void OnPos2Y(OSCMessage message) { OnPosY(data[1], message); }
+
+    void OnPos3X(OSCMessage message) { OnPosX(data[2], message); }
+    void OnPos3Y(OSCMessage message) { OnPosY(data[2], message); }
+
+
+    void OnPosX(ObjectData d,  OSCMessage message)
+    {
+        d.pos.x = (int)message.Values[0].IntValue;
+        CheckPos(d);
+    }
+    void OnPosY(ObjectData d, OSCMessage message)
+    {
+        d.pos.y = (int)message.Values[0].IntValue;
+        CheckPos(d);
+    }
+    public void CheckPos(ObjectData d)
+    {
+        print("CheckPos " + d.pos.x + " : " + d.pos.y);
+        if (d.pos.x == 0 || d.pos.y == 0) return;
+        if (d.last_pos_timer + filterDuration > Time.time) return;
+        //if (d.last_pos == Vector2.zero || Vector2.Distance(d.last_pos, d.pos) > offset)
+        //{
+            d.last_pos = d.pos;
+            d.last_pos_timer = Time.time;
+            inputManager.OnHit(d.pos);
+            field.text = d.pos.ToString();
+            d.pos = Vector2.zero;
+       // }
+    }
+    //receiver.Bind("/" + key + 1 + "x", OnPos1X);
+    //    receiver.Bind("/" + key + 1 + "y", OnPos1Y);
+    //public Vector2 pos_1;
+    //public Vector2 last_pos_1;
+    //float last_pos_1_timer;
+    //void OnPos1X(OSCMessage message)
     //{
-    //    print("OSCExampleReceiver" + message);
-    //    if (message.Values.Count > 0)
+    //    pos_1.x = (int)message.Values[0].IntValue;
+    //    CheckPos1();
+    //}
+    //void OnPos1Y(OSCMessage message)
+    //{
+    //    pos_1.y = (int)message.Values[0].IntValue;
+    //    CheckPos1();
+    //}
+    //void CheckPos1()
+    //{
+    //    if (pos_1.x == 0 || pos_1.y == 0) return;
+    //    if (last_pos_1_timer + filterDuration > Time.time) return;
+    //    if (last_pos_1 == Vector2.zero || Vector2.Distance(last_pos_1, pos_1) > offset)
     //    {
-    //        var value = message.Values[0].FloatValue; // O .IntValue, .StringValue, etc.
-    //       // Debug.Log("Mensaje OSC recibido: " + value);
-    //        field.text = value.ToString();
-    //        pos_1.x = value;
+    //        last_pos_1 = pos_1;
+    //        last_pos_1_timer = Time.time;
+    //        inputManager.OnHit(pos_1);
+    //        field.text = pos_1.ToString();
+    //        pos_1 = Vector2.zero;
     //    }
     //}
-
 }
